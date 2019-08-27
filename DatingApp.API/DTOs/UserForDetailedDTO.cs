6f1b0b98f41f312c4f5561a1231ee691c0f992d8
NaiKo <nicodauch@gmail.com>
2019-08-26 21:42:55 -0400

    End of Section 9

--- DatingApp.API/DTOs/UserForDetailedDTO.cs
+++ DatingApp.API/DTOs/UserForDetailedDTO.cs
@@ -10,6 +10,7 @@ namespace DatingApp.API.DTOs
         public string Username { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
+        public string KnowsAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Introduction { get; set; }
